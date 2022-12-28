b94f73fe9cbc2bd80f24e3db25cb032252f38529
Joakim Rosenstam <joakim.rosenstam@gmail.com>
2022-12-28 18:57:35 +0100

    remove silly deps, dont use netlify-edge

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   css: ["~/assets/css/main.css"],
-  nitro: {
-    preset: "netlify-edge",
-  },
   postcss: {
     plugins: {
       tailwindcss: {},
